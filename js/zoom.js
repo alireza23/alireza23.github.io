e016139a76f32c70c198d35556c517ec28476f61
Ali <ali.haghighi1367@gmail.com>
2020-04-28 23:06:55 +0430

    c60

--- js/zoom.js
+++ js/zoom.js
@@ -195,15 +195,15 @@ function update() {
   if(current.x > (current.width - originalSize.width)/2){
     current.x = (current.width - originalSize.width)/2
 }
-// if(current.y < - ((current.height - originalSize.height)/2)){
-//     current.y = - ((current.height - originalSize.height)/2)
-// }
+if(current.y < - ((current.height - originalSize.height)/2)){
+    current.y = - ((current.height - originalSize.height)/2)
+}
 // if(current.y > (current.height - originalSize.height)/2){
 //   current.y = (current.height - originalSize.height)/2
 // }
-if(current.y < 0){
-    current.y = 0
-}
+// if(current.y < 0){
+//     current.y = 0
+// }
 
 console.log({zoom, x: current.x, w: current.width, y: current.y, h: current.height})
   //console.log(`x  =  ${current.x}, y = ${current.y}`)
