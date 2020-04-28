270d2053c11a5a02df1c4c46edbc0fbb183093a5
Ali <ali.haghighi1367@gmail.com>
2020-04-28 18:26:23 +0430

    single pan removed

--- js/zoom.js
+++ js/zoom.js
@@ -1,4 +1,6 @@
 var element = document.getElementById('container')
+var width = element.offsetWidth;
+var height = element.offsetHeight;
     var hammertime = new Hammer(element, {});
 
     hammertime.get('pinch').set({ enable: true });
@@ -9,8 +11,8 @@ var element = document.getElementById('container')
     var lastEvent = undefined;
 
     var originalSize = {
-        width: 200,
-        height: 300
+        width: width,
+        height: height
     }
 
     var current = {
@@ -115,18 +117,20 @@ var element = document.getElementById('container')
     })
 
     hammertime.on('pan', function(e) {
-        console.log
-        if (lastEvent !== 'pan') {
-            fixHammerjsDeltaIssue = {
-                x: e.deltaX,
-                y: e.deltaY
+        if(e.maxPointers ==2){
+            if (lastEvent !== 'pan') {
+                fixHammerjsDeltaIssue = {
+                    x: e.deltaX,
+                    y: e.deltaY
+                }
             }
+    
+            current.x = last.x + e.deltaX - fixHammerjsDeltaIssue.x;
+            current.y = last.y + e.deltaY - fixHammerjsDeltaIssue.y;
+            lastEvent = 'pan';
+            update();
         }
-
-        current.x = last.x + e.deltaX - fixHammerjsDeltaIssue.x;
-        current.y = last.y + e.deltaY - fixHammerjsDeltaIssue.y;
-        lastEvent = 'pan';
-        update();
+      
     })    
 
     hammertime.on('pinch', function(e) {
@@ -147,9 +151,12 @@ var element = document.getElementById('container')
     })
 
     hammertime.on('panend', function(e) {
-        last.x = current.x;
-        last.y = current.y;
-        lastEvent = 'panend';
+        if(e.maxPointers ==2){
+            last.x = current.x;
+            last.y = current.y;
+            lastEvent = 'panend';
+        }
+    
     })
 
     hammertime.on('pinchend', function(e) {
